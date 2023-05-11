161d0340f7d38381bac681df199e92b48a11853b
romainthorrand <rom.thorrand@gmail.com>
2023-05-11 13:55:45 +0200

    Creation de la page d'accueil

--- front/js/script.js
+++ front/js/script.js
@@ -2,13 +2,33 @@ fetch("http://127.0.0.1:3000/api/products")
 .then (res => res.json ())
 .then (data => { affiche (data) })
 
-
+// Fonction qui affiche le nom des canapés
 function affiche (canapes) {
 
-    console.log (canapes)
-    const section = document.getElementById ("items") 
+    console.log (canapes);
+    const container = document.getElementById ("items") 
     canapes.forEach(element => {
-        section.innerHTML += `<h3>${element.name}</h3>`  
+        container.innerHTML += `<a href="./product.html?id=42">
+        <article>
+          <img src="${element.imageUrl}" alt="${element.altTxt}">
+          <h3 class="productName">${element.name}</h3>
+          <p class="productDescription">${element.description}</p>
+        </article>
+      </a>`  
     });
 }
 
+
+// fetch("http://127.0.0.1:3000/api/products")
+// .then (res => res.json ())
+// .then (data => { affiche (data) })
+
+// // Fonction qui affiche le nom des canapés
+// function affiche (canapes) {
+
+//     console.log (canapes);
+//     const container = document.getElementById ("items") 
+//     canapes.forEach(element => {
+//         container.innerHTML += ``  
+//     });
+// }
