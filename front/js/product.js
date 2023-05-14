2dfed649865cca8839c113d219967c41056840ab
romainthorrand <rom.thorrand@gmail.com>
2023-05-14 18:43:09 +0200

    Ajout des titres,descriptions, images des pages produits

--- front/js/product.js
+++ front/js/product.js
@@ -13,8 +13,20 @@ const getArticle = () => {
         return res.json()
     })
     .then(function(data){
-        console.log(data)
+        console.log(data);
+        // Ajout des titre,description, prix et image des canapés dans les pages produit
+        const title = document.getElementById("title").innerHTML = data.name;
+        const description = document.getElementById("description").innerHTML = data.description;
+        const price = document.getElementById("price").innerHTML = data.price;
+        //Création d'un attribut pour mettre l'image des canapé
+        const image = document.createElement("img");
+        document.querySelector(".item__img").appendChild(image);
+        image.setAttribute("src",`${data.imageUrl}`);
+
+
     })
 }
 
 getArticle();
+
+
